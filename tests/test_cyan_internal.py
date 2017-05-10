e761de539b1cc87977cddff64b63a63c411f2a2e
Tom Purucker <purucker.tom@gmail.com>
2017-05-10 21:00:16 +0000

    updated png endpoint

--- tests/test_cyan_internal.py
+++ tests/test_cyan_internal.py
@@ -11,7 +11,9 @@ pages = ["", "map", "lakecomparison", "dashboard", "algorithms", "references"]
 
 api_endpoints = ["https://cyan.epa.gov/cyan/cyano/location/data/28.6138/-81.6227/2017-12-08",
                  "https://cyan.epa.gov/cyan/cyano/notifications/2015-05-03T20-16-26-000-0400",
-                 "https://cyan.epa.gov/cyan/cyano/location/images/envisat.2012097.0406.1541C.L3.EF3.v670.CIcyano1.png",
+                 #if the next png 500s, you can get an updated image from a specific location e.g.,
+                 #https://cyan.epa.gov/cyan/cyano/location/images/28.6138/-81.6227/
+                 "https://cyan.epa.gov/cyan/cyano/location/images/envisat.2012094.0403.1551C.L3.EF3.v670.CIcyano2.png",
                  "https://cyan.epa.gov/cyan/cyano/location/images/28.6138/-81.6227/"]
 
 #following are lists of url's to be processed with tests below
