32632201c616dd2e24ade7c80bedf8ff2a176583
Tom Purucker <purucker.tom@gmail.com>
2017-07-03 16:28:49 +0000

    consistent imports

--- urls.py
+++ urls.py
@@ -6,31 +6,25 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 
-from .views import file_not_found
-from .description import description_page
-from .map import map_page
-from .lakecomparison import lakecomparison_page
-from .dashboard import dashboard_page
-from .algorithms import algorithm_page
-from .references import references_page
-from .cyan_rest import get_location_data
+from . import views, description, map, lakecomparison
+from . import dashboard, algorithms, references, cyan_rest
 
 # if settings.IS_PUBLIC:
 urlpatterns = [
     # url(r'^$', views.cyan_landing_page),
     #front end urls
-    url(r'^$', description_page, {'model': 'cyan'}),
-    url(r'^map$', map_page, {'model': 'cyan'}),
-    url(r'^lakecomparison$', lakecomparison_page, {'model': 'cyan'}),
-    url(r'^dashboard$', dashboard_page, {'model': 'cyan'}),
-    url(r'^algorithms$', algorithm_page, {'model': 'cyan'}),
-    url(r'^references$', references_page, {'model': 'cyan'}),
+    url(r'^$', description.description_page, {'model': 'cyan'}),
+    url(r'^map$', map.map_page, {'model': 'cyan'}),
+    url(r'^lakecomparison$', lakecomparison.lakecomparison_page, {'model': 'cyan'}),
+    url(r'^dashboard$', dashboard.dashboard_page, {'model': 'cyan'}),
+    url(r'^algorithms$', algorithms.algorithm_page, {'model': 'cyan'}),
+    url(r'^references$', references.references_page, {'model': 'cyan'}),
 
     # url(r'^api$', rest.rest_page, {'model': 'cyan'}),
     # url(r'^swag$', views.getSwaggerJsonContent)
 
     # rest urls
-    url(r'^rest/location/data$', get_location_data)
+    url(r'^rest/location/data$', cyan_rest.get_location_data)
 ]
 # else:
 #     urlpatterns = [
@@ -42,8 +36,8 @@ urlpatterns = [
 #     ]
 
 # 404 Error view (file not found)
-handler404 = file_not_found
+handler404 = views.file_not_found
 # 500 Error view (server error)
-handler500 = file_not_found
+handler500 = views.file_not_found
 # 403 Error view (forbidden)
-handler403 = file_not_found
+handler403 = views.file_not_found
