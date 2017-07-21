316f7cce73f859ac857d637febf706d0a0f527b1
AIgnatius <arignatius@gmail.com>
2017-07-21 15:54:55 -0400

    add cyano "dot map" of usa timeseries

---
+++ dot_map.py
@@ -0,0 +1,26 @@
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from django.shortcuts import redirect
+import os
+from django.conf import settings
+from .links_left import ordered_list
+from cyan_app import views
+
+
+
+def dotmap_page(request, model='cyan', header='none'):
+
+    """ Returns the html of the references page for cyan. """
+    html = render_to_string('01epa_drupal_header.html', {})
+    html += render_to_string('02epa_drupal_header_bluestripe_wide.html', {})
+
+    html += render_to_string('cyan_dot_map_overtime.html', {})
+
+    html += render_to_string('04ubertext_end_drupal.html', {})
+
+    html += render_to_string('10epa_drupal_footer.html', {})
+
+
+    response = HttpResponse()
+    response.write(html)
+    return response
--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ Definition of urls for qed_cyan.
 from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
-from . import views, description, map, lakecomparison
+from . import views, description, map, lakecomparison, dot_map
 from . import dashboard, algorithms, references, cyan_rest, cyandata_restapi
 
 
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^$', description.description_page, {'model': 'cyan'}),
     url(r'^map$', map.map_page, {'model': 'cyan'}),
     url(r'^lakecomparison$', lakecomparison.lakecomparison_page, {'model': 'cyan'}),
+    url(r'^dot_map', dot_map.dotmap_page, {'model': 'cyan'}),
     url(r'^dashboard$', dashboard.dashboard_page, {'model': 'cyan'}),
     url(r'^algorithms$', algorithms.algorithm_page, {'model': 'cyan'}),
     url(r'^references$', references.references_page, {'model': 'cyan'}),
