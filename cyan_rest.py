94ac0f95a6b7fb87f0c23319ff0c5015fca37e2d
dbsmith88 <dbsmith@uga.edu>
2018-01-24 13:04:07 -0600

    update to django 2.0 urls, fixed api rest call

--- cyan_rest.py
+++ cyan_rest.py
@@ -1,28 +1,23 @@
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import os
 import json
 import requests
 
 
+@csrf_exempt
 def get_location_data(request):
     """
     get cyan data by lat long location
     """
-    print('Inside get_location_data')
-    baseURL = os.getenv('CYAN_REST_SERVER')
-    print(baseURL)
+    baseURL = 'https://cyan.epa.gov/cyan/cyano'
+    # baseURL = os.getenv('CYAN_REST_SERVER')
     data = (request.POST)
-    print(data)
     latitude = data['latitude']
-    print(latitude)
     longitude = data['longitude']
-    print(longitude)
     date = data['date']
-    print(date)
     url = "{0}/location/data/{1}/{2}/{3}".format(baseURL, latitude, longitude, date)
-    print(url)
+    print('Cyan REST data request. Latitude: ' + str(latitude) + ', Longitude: ' + str(longitude) + ', Date: ' + str(date))
     resp = requests.get(url)
     resp2 = HttpResponse(resp.content, content_type='application/json')
-    print(resp2)
-    print(resp.content)
     return resp2
